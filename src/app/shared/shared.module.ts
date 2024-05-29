e3c9f79571be4f2ea28ee49cacce8afefc4a734b
rainiery valerio <rainiery.valerio@economia.gob.do>
2024-05-29 15:52:06 -0400

    manual entries form created and stylized

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,23 +6,30 @@ import { FileUploadModule } from 'ng2-file-upload';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { FileExportComponent } from './components/file-export/file-export.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ManualEntriesFormComponent } from './components/manual-entries-form/manual-entries-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
     FileUploadModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgbCollapseModule
   ],
   declarations: [
     ModalComponent,
     FileUploadComponent,
-    FileExportComponent
+    FileExportComponent,
+    ManualEntriesFormComponent
   ],
   exports: [
     ModalComponent,
     FileUploadComponent,
-    FileExportComponent
+    FileExportComponent,
+    ManualEntriesFormComponent
   ]
 })
 export class SharedModule { }

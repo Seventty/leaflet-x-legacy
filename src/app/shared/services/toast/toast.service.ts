28baf18a2d078f76c7e4d5b134b4cae9382b4cfd
rainiery valerio <rainiery.valerio@economia.gob.do>
2024-05-29 12:27:45 -0400

    old toast service code wiped

--- src/app/shared/services/toast/toast.service.ts
+++ src/app/shared/services/toast/toast.service.ts
@@ -5,58 +5,6 @@ import Swal, { SweetAlertIcon, SweetAlertOptions } from 'sweetalert2';
   providedIn: 'root'
 })
 export class ToastService {
-  /* private toastText: string = '';
-  private toastIcon: SweetAlertIcon = 'success';
-  private toastTitle: string = '';
-
-  private toastObject: SweetAlertOptions = {
-    text: this.toastText,
-    icon: this.toastIcon,
-    title: this.toastTitle,
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    showCloseButton: true,
-    timer: 5000,
-    timerProgressBar: true,
-    didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-  }
-
-  constructor() { }
-
-  public errorToast(title: string, text: string = ''){
-    this.toastObject.title = title
-    this.toastObject.icon = "error";
-    this.toastObject.text = text;
-    Swal.fire(this.toastObject)
-  }
-
-  public successToast(title: string, text: string = ''){
-    this.toastObject.title = title
-    this.toastObject.icon = "success";
-    this.toastObject.text = text;
-    Swal.fire(this.toastObject)
-  }
-
-  public infoToast(title: string, text: string = ''){
-    this.toastObject.title = title
-    this.toastObject.icon = "info";
-    this.toastObject.text = text;
-    this.toastObject.html = text;
-    Swal.fire(this.toastObject)
-  }
-
-  public warningToast(title: string, text: string = ''){
-    this.toastObject.title = title
-    this.toastObject.icon = "warning";
-    this.toastObject.text = text;
-    this.toastObject.html = text;
-    Swal.fire(this.toastObject)
-  } */
-
   private defaultOptions: SweetAlertOptions = {
     toast: true,
     position: 'top-end',

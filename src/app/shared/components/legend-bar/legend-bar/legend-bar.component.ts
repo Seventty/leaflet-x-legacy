62dfeb904b79cb4b560050abf1f9dd0902d68b68
rainieryeconomia <rainiery.valerio@economia.gob.do>
2024-06-14 17:00:50 -0400

    feat: legend bar map initialized

---
+++ src/app/shared/components/legend-bar/legend-bar/legend-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'legend-bar',
+  templateUrl: './legend-bar.component.html',
+  styleUrls: ['./legend-bar.component.sass']
+})
+export class LegendBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

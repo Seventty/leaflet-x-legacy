310810583c5139b5c9f3bb1763a1aa0f95bae2ed
keury.lendof <keury.lendof@economia.gob.do>
2024-07-10 13:41:59 -0400

    maquetacion puntos

--- projects/leaflet-x/src/lib/shared/components/widgets/manual-form/components/point-form/point-form.component.ts
+++ projects/leaflet-x/src/lib/shared/components/widgets/manual-form/components/point-form/point-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-point-form',
@@ -6,10 +7,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./point-form.component.sass']
 })
 export class PointFormComponent implements OnInit {
+  points = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addNewPoint() {
+    const newPoint = { longitud: '', latitud: '' };
+    this.points.push(newPoint);
+    console.log('Points after addition:', this.points);
+  }
+
+  deletePoint(index: number) {
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: `¿Quieres eliminar el vértice ${index + 1}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminarlo'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.points.splice(index, 1);
+        console.log('Points after deletion:', this.points);
+        Swal.fire(
+          'Eliminado',
+          'El vértice ha sido eliminado correctamente.',
+          'success'
+        );
+      }
+    });
+  }
+
+  handlePointChange(index: number, point: any) {
+    this.points[index] = point;
+    console.log('Updated points:', this.points);
   }
 
 }
